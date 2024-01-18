452898064b702713a23523253df6c5a3e9ac60ba
Hubert <142225610+hubertrokosz@users.noreply.github.com>
2024-01-18 20:57:46 +0100

    Created html calculator and basic math functions in js

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(op, num1, num2) {
+    switch(op) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
