3ef3b5417b1a1213d3fe4268ca5c6895974ade90
Hubert <142225610+hubertrokosz@users.noreply.github.com>
2024-01-19 22:25:43 +0100

    Made a working calculator ability to add, divide, multiply and divide

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let firstNum;
 let secondNum;
 let operator;
 
-function operate(op, num1, num2) {
+function operate(num1, op, num2) {
     switch(op) {
         case "+":
             return add(num1, num2);
@@ -36,13 +36,49 @@ function operate(op, num1, num2) {
 }
 
 let displayValue;
+let arr = [];
+let result;
+
 
 let numButtons = document.querySelectorAll(".num");
 let screen = document.querySelector("#screen-content");
+let clear = document.querySelector("#clear");
+let equals = document.querySelector("#equals");
+
+clear.addEventListener("click", () => {
+    screen.textContent = "0";
+    displayValue = 0;
+});
 
 numButtons.forEach(element => {
     element.addEventListener("click", () => {
-        //console.log(element.textContent);
-        screen.textContent += element.textContent;
+        if (screen.textContent == "0" || ((element.textContent >= "0" && element.textContent <= "9") && screen.textContent == result)) screen.textContent = element.textContent;
+        else {
+            screen.textContent += element.textContent;
+            displayValue = screen.textContent;
+            arr = displayValue.split((/([\+\-\*\/])/));
+        }
+        
     })
 });
+
+equals.addEventListener("click", () => {
+    while (arr.length >= 3) {
+        result = arr.splice(0, 3);
+        result = operate(parseInt(result[0]), result[1], parseInt(result[2]));
+        arr.unshift(result);
+    }
+
+    if (result == Infinity) {
+        alert("Don't divide by zero!");
+        result = "";
+    }
+    if (isFloatOrInteger(result)) screen.textContent = result.toFixed(2);
+    screen.textContent = result;
+    
+});
+
+function isFloatOrInteger(number) {
+    return Math.abs(number - Math.round(number)) > Number.EPSILON;
+  }
+
